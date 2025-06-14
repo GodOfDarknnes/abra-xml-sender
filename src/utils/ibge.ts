fb3c53da4a9e274d6d7d9cc35a8a687c861dac80
Jonathan Guimaraes <167234862+GodOfDarknnes@users.noreply.github.com>
2025-06-14 14:58:52 -0300

    Update ibge.ts

--- src/utils/ibge.ts
+++ src/utils/ibge.ts
@@ -15,23 +15,28 @@ export async function getMunicipioNomeUf(codigo: string): Promise<string> {
     console.log("Buscando município na API IBGE, código:", codigo);
     const url = `https://servicodados.ibge.gov.br/api/v2/municipios/${codigo}`;
     const res = await fetch(url);
+
     console.log("Status IBGE fetch:", res.status, res.statusText);
+
     if (!res.ok) {
       console.error("Resposta não-ok do IBGE:", res.status, res.statusText);
-      throw new Error(`IBGE não retornou OK`);
+      throw new Error("IBGE não retornou OK");
     }
-    const data: MunicipioIBGE[] = await res.json();
+
+    const data = await res.json();
+
     console.log("Resposta JSON IBGE:", data);
-    // API retorna array com 1 elemento ou erro
+
     if (Array.isArray(data) && data.length > 0) {
       const municipio = data[0];
       const nome = municipio.nome;
       const uf = municipio.microrregiao.mesorregiao.UF.sigla;
-      console.log("Municipio encontrado:", `${nome}/${uf}`);
+      console.log("Município encontrado:", `${nome}/${uf}`);
       return `${nome}/${uf}`;
+    } else {
+      console.error("Array IBGE vazio ou inválido!", data);
+      return "";
     }
-    console.error("Array IBGE vazio ou inválido!", data);
-    return "";
   } catch (err) {
     console.error("Erro ao buscar município IBGE:", err);
     return "";
